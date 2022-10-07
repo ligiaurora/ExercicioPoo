5a4cad27f3a024d1b162f427abc41c95caac4af3
ligia <ligia@DESKTOP-33DDFM8>
2022-10-06 23:34:31 -0300

    commit exercicio

--- ExerciciosPoo/src/ex3/Main.java
+++ ExerciciosPoo/src/ex3/Main.java
@@ -6,6 +6,15 @@ public class Main {
 		
 		Scanner entrada = new Scanner(System.in);
 		
+		ex3 n1 = new ex3();
+		System.out.print("Informe o valor de primeiro número:");
+		n1.setNum1(entrada.nextDouble());
+		
+		
+		ex3 n2 = new ex3();
+		System.out.print("Informe o valor de um segundo número: ");
+		n2.setNum2(entrada.nextDouble());
+		
 		
 		
 		entrada.close();
