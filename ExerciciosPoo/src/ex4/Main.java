32c2e4c183a3a6abfec7de735e790b29eef60a71
ligia <ligia@DESKTOP-33DDFM8>
2022-10-12 02:00:55 -0300

    exercicio 4

---
+++ ExerciciosPoo/src/ex4/Main.java
@@ -0,0 +1,24 @@
+package ex4;
+import java.util.Scanner;
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		//ex1 nc = new ex1();
+		System.out.print("Informe o seu nome:");
+		//nc.setNome(entrada.next());
+		System.out.print("Informe o seu sobrenome:");
+		//nc.setSobrenome(entrada.next());
+		
+		
+		System.out.println("");
+		
+		entrada.close();
+
+	}
+
+}
